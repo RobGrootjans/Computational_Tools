568405429e1d4dc81e6be19ddc83f5ffd4379af5
Mats1416 <148191373+Mats1416@users.noreply.github.com>
2023-12-15 16:03:36 +0100

    Update Old_system.py

--- Old_system.py
+++ Old_system.py
@@ -24,9 +24,10 @@ def schedule_appointment(machine, patient, slot_duration):
     if patient.scan_duration > slot_duration:
         machine.surplus[patient.appointment_date] += patient.scan_duration - slot_duration   # difference between actual duration and slot time = surplus
 
-def generate_data(num_days, start_time, end_time, mean_num_type1, mean_num_type2,
-                  mean_duration_type1, shape_duration_type2, std_duration_type1,
-                  scale_duration_type2, lambda_tbc_type1, mean_tbc_type2, std_tbc_type2):
+def generate_data(num_days, start_time, end_time, mean_num_type1, mean_duration_type1, 
+                  std_duration_type1, lambda_tbc_type1, mean_num_type2, 
+                  std_num_type2, mean_duration_type2, std_duration_type2, 
+                  mean_tbc_type2, std_tbc_type2):
     
     np.random.seed(42) # for reproducibility
     data = []
@@ -53,7 +54,7 @@ def generate_data(num_days, start_time, end_time, mean_num_type1, mean_num_type2
         current_time = start_time
         
         while current_time < end_time:
-            num_type2 = np.random.poisson(mean_num_type2)
+            num_type2 = int(np.round(np.random.normal(mean_num_type2, std_num_type2)))
             call_intervals_type2 = np.random.normal(mean_tbc_type2, std_tbc_type2, num_type2)
             for interval in call_intervals_type2:
                 current_time += interval
@@ -61,7 +62,7 @@ def generate_data(num_days, start_time, end_time, mean_num_type1, mean_num_type2
                     call_date = day
                     call_time = current_time
                     category = 2
-                    scan_duration = np.random.gamma(shape_duration_type2, scale_duration_type2)
+                    scan_duration = np.random.normal(mean_duration_type2, std_duration_type2)
                     data.append([call_date, call_time, category, None, None, scan_duration])
                 else:
                     break
@@ -92,18 +93,19 @@ end_time = 17
 # For type 1:
 # Number of patients per day: poisson-distr. with mean 16.47826
 mean_num_type1 = 16.47826
-# Duration of a scan: normally distributed with mean 0.4326608 and sd 0.09777424
+# Duration of a scan: normally distributed with mean 0.4326608 and std 0.09777424
 mean_duration_type1 = 0.4326608
 std_duration_type1 = 0.09777424
 # Time between calls: exponential distribution with lambda 1.835622
 lambda_tbc_type1 = 1.835622         # tbc = time between calls
 
 # For type 2:
-# Number of patients a day: unknown
-mean_num_type2 = 12.87234
-# Durations of a scan: gamma distribution with  shape 1.136300 and scale 1.731915
-shape_duration_type2 = 1.136300 
-scale_duration_type2 = 1.731915
+# Number of patients a day: normally distributed with mean 10.383 and std 1.1960
+mean_num_type2 = 10.383
+std_num_type2 = 1.1960
+# Durations of a scan: normally distributed with mean 0.6693389 and std 0.1872859
+mean_duration_type2 = 0.6693389 
+std_duration_type2 = 0.1872859
 # Time between calls: normal distribution with mean 0.8666245 and Std 0.3114448
 mean_tbc_type2 = 0.8666245
 std_tbc_type2 = 0.3114448
@@ -116,9 +118,10 @@ machine2 = Machine()
 slot_duration_t1 = 0.2
 slot_duration_t2 = 0.2
 
-data_df = generate_data(num_days, start_time, end_time, mean_num_type1, mean_num_type2,
-                        mean_duration_type1, shape_duration_type2, std_duration_type1,
-                        scale_duration_type2, lambda_tbc_type1, mean_tbc_type2, std_tbc_type2)
+data_df = generate_data(num_days, start_time, end_time, mean_num_type1, mean_duration_type1,
+                        std_duration_type1, lambda_tbc_type1, mean_num_type2,
+                        std_num_type2, mean_duration_type2, std_duration_type2,
+                        mean_tbc_type2, std_tbc_type2)
 
 # Creating instances and sorting patients acoording to their call time to apply FCFS
 patients = []
